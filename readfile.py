ba5daff4c99e512b159dfd63353b4d3f0b932ffa
Barbara Zambiasi Martinelli <martinelli.bz@gmail.com>
2017-09-30 17:14:01 +0100

    Update readfile.py

--- readfile.py
+++ readfile.py
@@ -1,4 +1,3 @@
-
 # Reading the file:
 def read_input_file(file_argv, start_coordinate, window):
     all_positions = {} # all CpGs as keys
@@ -9,19 +8,13 @@ def read_input_file(file_argv, start_coordinate, window):
         for line in file:
             fields = line.replace('\n','').split('\t')
             position = int(fields[0]) # each position corresponds to one CpG
+            read = fields[2]
+            methylation = str(fields[1])
             if (position >= start_coordinate and position < (start_coordinate + window)):
                 # if the CpG is in the genomic locus of interest, those 3 dictionaries initialized above are created.
                 all_positions[position] = 1
                 all_reads[fields[2]] = 1
-                meth_status.setdefault(int(fields[0]), {}).setdefault(fields[2], []).append(str(fields[1]))
+                meth_status.setdefault(position, {}).setdefault(read, []).append(methylation)
     n_cpgs = len(all_positions.keys()) # total number of CpGs
     print 'Number of CpG sites: ', n_cpgs
     return all_reads, all_positions, meth_status, n_cpgs
-
-
-
-
-
-
-
-
