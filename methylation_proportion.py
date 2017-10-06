3aa1457ca551aef555f6b65bf3d29b5a84306eca
Barbara Zambiasi Martinelli <martinelli.bz@gmail.com>
2017-10-06 14:41:49 +0100

    Update methylation_proportion.py

--- methylation_proportion.py
+++ methylation_proportion.py
@@ -1,10 +1,7 @@
-# Methylation Frequency calculation
-
 
 # Calculate the proportion of the CpGs in a methylated state.
+
 def meth_freqs(all_positions, meth_status, hap_freq, min_threshold, file_argv):
-    methfreq = open(file_argv+"_Proportion_Methylated_CpGs", "w")
-    methfreq.write('Position\tFrom reads\tFrom haplotypes\n')
     
     # Proportion of methylated CpGs calculated using the sequence reads
     proportion_from_reads = {} # proportion of methylated CpGs in each position
@@ -26,11 +23,5 @@ def meth_freqs(all_positions, meth_status, hap_freq, min_threshold, file_argv):
                 if hap_cpg[position] == '1':
                     meth_proportion[position] += float(hap_freq[hap]) # sum of the frequencies of all haplotypes containing the given CpG in a methylated state
             proportion_from_haps = meth_proportion
-
-    # Writing the output file containing the proportion of methylated CpGs based on the calculation using the sequence reads or the haplotype frequencies
-    for cpg1 in proportion_from_reads.keys():
-        for cpg2 in proportion_from_haps.keys():
-            if cpg1 == cpg2:
-                methfreq.write(str(cpg1)+'\t'+str(proportion_from_reads[cpg1])+'\t'+str(proportion_from_haps[cpg2])+'\n')
-
-
+            
+    return proportion_from_reads, proportion_from_haps
