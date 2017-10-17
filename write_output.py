bf6272b5aa8de9442c3a57b8d95138a374ae60fa
Barbara Zambiasi Martinelli <martinelli.bz@gmail.com>
2017-10-17 13:32:08 +0100

    Update write_output.py

--- write_output.py
+++ write_output.py
@@ -10,13 +10,13 @@ def output_freq_entropy(hap_freq, min_threshold, entropy, file_argv):
 
 
 
-# output file containing the proportion of each CpG based on the calculation using the sequence reads or the haplotype frequencies
-def output_meth(proportion_from_reads, proportion_from_haps, file_argv):
-    methout = open(file_argv+"_Proportion_Methylated_CpGs", "w")
-    methout.write('Position\tFrom reads\tFrom haplotypes\n')
-    for cpg1 in proportion_from_reads.keys():
-        for cpg2 in proportion_from_haps.keys():
-            if cpg1 == cpg2:
-                methout.write(str(cpg1)+'\t'+str(proportion_from_reads[cpg1])+'\t'+str(proportion_from_haps[cpg2])+'\n')
+# # output file containing the proportion of each CpG based on the calculation using the sequence reads or the haplotype frequencies
+# def output_meth(proportion_from_reads, proportion_from_haps, file_argv):
+#     methout = open(file_argv+"_Proportion_Methylated_CpGs", "w")
+#     methout.write('Position\tFrom reads\tFrom haplotypes\n')
+#     for cpg1 in proportion_from_reads.keys():
+#         for cpg2 in proportion_from_haps.keys():
+#             if cpg1 == cpg2:
+#                 methout.write(str(cpg1)+'\t'+str(proportion_from_reads[cpg1])+'\t'+str(proportion_from_haps[cpg2])+'\n')
 
 
