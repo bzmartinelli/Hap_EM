3a0cc39794b16c16aa0f989273aa80b008ab950e
B Zambiasi Martinelli <martinelli.bz@gmail.com>
2018-04-12 17:05:39 +0100

    Update MHap.py

--- scripts/MHap.py
+++ scripts/MHap.py
@@ -21,11 +21,23 @@ args = parser.parse_args()
 
 command_line = " ".join(argv[1:])
 cwd = os.getcwd()
+
+if str(cwd) in args.cpg_reads_data:
+    cpg_reads_data = args.cpg_reads_data
+else:
+    cpg_reads_data = cwd+'/'+args.cpg_reads_data
+
+if str(cwd) in args.genomic_regions:
+    print '\n\nyes\n\n'
+    genomic_regions = args.genomic_regions
+else:
+    genomic_regions = cwd+'/'+args.genomic_regions
+
 os.chdir(path[0])
 
-selected_cpgs, id_windows = open_genomic_regions(args.genomic_regions, args.cpg_reads_data, args.data_from, args.meth_min, args.meth_max, args.window_size, args.number_of_cpgs)
+selected_cpgs, id_windows = open_genomic_regions(genomic_regions, cpg_reads_data, args.data_from, args.meth_min, args.meth_max, args.window_size, args.number_of_cpgs)
 
-writeout(args.cpg_reads_data, id_windows, selected_cpgs, args.data_from, command_line)
+writeout(cpg_reads_data, id_windows, selected_cpgs, args.data_from, command_line)
 
 os.system("python call.py "+ command_line + " -f new_cpg_reads " + " -wf windows " + "-cwd "+cwd)
 
