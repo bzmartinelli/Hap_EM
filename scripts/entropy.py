b03c2db89cddcf6366928e6583b1bb25dab604cc
B Zambiasi Martinelli <martinelli.bz@gmail.com>
2018-08-23 10:29:35 +0100

    Update entropy.py

--- scripts/entropy.py
+++ scripts/entropy.py
@@ -3,10 +3,9 @@ from numpy import *
 # Shannon entropy is used to calculate the methylation entropy
 def entropy(hap_freq, threshold_min_freq, n_cpgs):
     entropy = 0
-    for h, v in sorted(hap_freq.iteritems()):
+    for h, v in sorted(hap_freq.items()):
         if hap_freq[h] > threshold_min_freq:
             log2f = float(log(hap_freq[h]))
             entropy = entropy - hap_freq[h] * log2f
     entropy = float((1.0 / n_cpgs)) * entropy
-#    print '\nEntropy:', entropy, '\n'
     return entropy
