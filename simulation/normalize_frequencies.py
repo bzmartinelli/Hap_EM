a250b3c27d4ac46aa0cf89e5ec12eed3fae16050
Barbara Zambiasi Martinelli <martinelli.bz@gmail.com>
2020-02-12 15:38:34 +0000

    Update normalize_frequencies.py

--- simulation/normalize_frequencies.py
+++ simulation/normalize_frequencies.py
@@ -1,4 +1,4 @@
-# python normalize_frequencies.py freqs_sorted ID
+# python normalize_frequencies.py freqs_sorted_file IDdepth
 
 # normalize haplotype frequencies
 from sys import *
@@ -6,6 +6,7 @@ from numpy import *
 
 
 freqfile = open(argv[1])
+depth = str(argv[2]) 
 
 header = freqfile.readline()
 simfreq = {}   # {id:{hap:freq, hap:freq}}
@@ -22,13 +23,15 @@ for k, v in estfreq.iteritems():
 #        if hapfreqs_sum != 0:
         v[k2] = float(str(v[k2]).strip("[]"))/hapfreqs_sum  # float(str(v[k2]).strip("[]")) transform [0.2] in 0.2
 
-outALL = open("Frequencies_"+argv[2]+"_sorted_normalized", "w")
+outALL = open("Frequencies_sorted_normalized_depth"+depth, "w")
 outALL.write('Haplotype\tSimulated_Frequency\tEstimated_Frequency\tSim_id\n')
-
 
 for k, v in estfreq.iteritems():
     for k2 in v.keys():
         outALL.write(str(k2)+'\t'+str(simfreq[k][k2]).strip("[]")+'\t'+str(v[k2]).strip("[]")+'\t'+ str(k)+'\n')
+
+
+
 
 
 
