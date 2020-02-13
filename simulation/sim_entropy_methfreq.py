cf71db6fb6e711660d6a565a7729544d8cc4cfb4
Barbara Zambiasi Martinelli <martinelli.bz@gmail.com>
2020-02-13 18:02:36 +0000

    Update sim_entropy_methfreq.py

--- simulation/sim_entropy_methfreq.py
+++ simulation/sim_entropy_methfreq.py
@@ -3,7 +3,7 @@
 from sys import *
 from numpy import *
 from decimal import Decimal
-
+import os
 
 hap_freq_file = open(argv[1])
 SimID = (argv[2])
@@ -15,19 +15,17 @@ for line in hap_freq_file:
     haps[fields[0]] = float(fields[1])
     haps_dec[fields[0]] = Decimal(fields[1])
 
-
-
 entropy = 0
 for f in haps.keys():
     log2f = float(log(haps[f])) / log(2)
     entropy = entropy - haps[f] * log2f
 
 N = len(list(f))
-#print "---N: ", N
 entropy = float((1.0 / N)) * entropy
-out = open(SimID+"_Simulated_Entropy", "w")
-out.write('Simulated_entropy\tSim_id\n')
-out.write(str(entropy)+'\t'+str(SimID)+'\n')
+
+o = open(SimID+"_Simulated_Entropy", "w")
+o.write('Simulated_entropy\tSim_id\n')
+o.write(str(entropy)+'\t'+str(SimID)+'\n')
 #print '\nEntropy:', entropy, '\n'
 
 
@@ -44,20 +42,24 @@ for hap in sorted(haps_dec.keys()):
         if get_hap[position] == '1':
             fd[position] += haps_dec[hap]
 
-for k, v in fd.iteritems():
+for k, v in fd.items():
     fd[k] = float(v)
 
 
-out2 = open(SimID+"_Simulated_Meth_Proportion", 'w')
-out2.write('Position\tProportion\tSim_id\n')
-for k, v in fd.iteritems():
-    out2.write(str(k)+'\t'+str(v)+'\t'+str(SimID)+'\n')
+o2 = open(SimID+"_Simulated_Meth_Proportion", 'w')
+o2.write('Position\tProportion\tSim_id\n')
+for k, v in fd.items():
+    o2.write(str(k)+'\t'+str(v)+'\t'+str(SimID)+'\n')
 ##
 
-out3 = open(SimID+"_Simulated_Frequencies", "w")
-out3.write('Haplotype\tSimulated_Frequency\tSim_id\n')
-for h, f in haps.iteritems():
-    out3.write(str(h)+'\t'+str(f)+'\t'+str(SimID)+'\n')
+o3 = open(SimID+"_Simulated_Frequencies", "w")
+o3.write('Haplotype\tSimulated_Frequency\tSim_id\n')
+#for h, f in sorted(haps.items(), key=lambda (k,v): (v,k), reverse=True):
+for h, f in sorted(haps.items(), key=lambda x: x[1], reverse=True):
+#for h, f in haps.items():
+    o3.write(str(h)+'\t'+str(f)+'\t'+str(SimID)+'\n')
 
+
+os.remove(argv[1])
 
 
